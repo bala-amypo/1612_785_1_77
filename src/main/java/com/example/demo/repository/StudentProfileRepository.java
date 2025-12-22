68b7df6c41d2b1084df69da6ab059b98166c3c5d
bala-amypo <bala@amypo.in>
2025-12-22 09:49:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentProfileRepository.java
+++ src/main/java/com/example/demo/repository/StudentProfileRepository.java
@@ -2,13 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.StudentProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+public interface StudentProfileRepository
+        extends JpaRepository<StudentProfile, Long> {
 
-@Repository
-public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
     Optional<StudentProfile> findByEnrollmentId(String enrollmentId);
-    Optional<StudentProfile> findByUserId(Long userId);
-    boolean existsByEnrollmentId(String enrollmentId);
 }
--- src/main/java/com/example/demo/service/Impl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentProfileServiceImpl.java
@@ -1,19 +1,39 @@
 package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.StudentProfile;
+import com.example.demo.repository.StudentProfileRepository;
 import com.example.demo.service.StudentProfileService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentProfileServiceImpl implements StudentProfileService {
 
+    @Autowired
+    private StudentProfileRepository repository;
+
     @Override
-    public StudentProfile getByUserId(Long userId) {
-        return null;
+    public StudentProfile createOrUpdateProfile(StudentProfile profile) {
+        return repository.save(profile);
+    }
+
+    @Override
+    public StudentProfile getProfileById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student profile not found"));
     }
 
     @Override
     public StudentProfile getProfileByEnrollmentId(String enrollmentId) {
-        return null;
+        return repository.findByEnrollmentId(enrollmentId)
+                .orElseThrow(() -> new RuntimeException("Student profile not found"));
+    }
+
+    @Override
+    public List<StudentProfile> getAllProfiles() {
+        return repository.findAll();
     }
 }
