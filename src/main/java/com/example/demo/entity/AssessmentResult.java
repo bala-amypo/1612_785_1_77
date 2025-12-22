38bb847808742a5e10cdc3bbfd26ff6dd009d362
bala-amypo <bala@amypo.in>
2025-12-22 08:52:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssessmentResult.java
+++ src/main/java/com/example/demo/entity/AssessmentResult.java
@@ -3,10 +3,14 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssessmentResult {
 
     @Id
