740c44d73c2d345c6aee849941d4fd3a5b73728e
bala-amypo <bala@amypo.in>
2025-12-22 09:10:49 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SkillServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.Skill;
 import com.example.demo.service.SkillService;
+import com.example.demo.repository.SkillServiceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
