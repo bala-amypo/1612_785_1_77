48cd6f199ba1f4683f691039c24679c6592b5c28
bala-amypo <bala@amypo.in>
2025-12-23 09:02:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillGapRecommendationRepository.java
+++ src/main/java/com/example/demo/repository/SkillGapRecommendationRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface SkillGapRecommendationRepository extends JpaRepository<SkillGapRecommendation, Long> {
-   // List<SkillGapRecommendation> findByStudentProfileIdOrderByGeneratedAtDesc(Long studentId);
-   List<SkillGapRecommendation> findByStudent_IdOrderByGeneratedAtDesc(Long studentId);
+List<SkillGapRecommendation> findByStudentId(Long studentId);
+
 
 }
